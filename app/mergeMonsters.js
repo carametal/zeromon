564bfaa8d366adb8a30d82448e3f4261e69f6378
mitsuki.fukuda <mitsuki.fukuda@g.smartvalue.ad.jp>
2024-03-20 13:25:01 +0900

    合成モンスター追加

--- app/mergeMonsters.js
+++ app/mergeMonsters.js
@@ -1,5 +1,6 @@
 export default mergeMonsters = [
   {
+    id: 4,
     name: "グリーンウィンディア",
     src: "../public/monsters/tamanegiFuryoku.png",
     feature:
@@ -12,7 +13,41 @@ export default mergeMonsters = [
     },
     mixup: {
       parent: "タマネギカボモン",
-      word: "風力発電"
+      word: "風力発電",
+    },
+  },
+  {
+    id: 5,
+    name: "リサイクリングストン",
+    src: "../public/monsters/tamanegiFuryoku.png",
+    feature:
+      "リサイクル愛溢れるガラクタ体のキュートストーン、地球を守るスマイルがチャームポイント！",
+    status: {
+      hp: { jp: "体力", value: 48300 },
+      def: { jp: "防御力", value: 77900 },
+      att: { jp: "攻撃力", value: 21500 },
+      special: { jp: "資源回復力", value: 89200 },
+    },
+    mixup: {
+      parent: "カーリングカボモン",
+      word: "リサイクル",
+    },
+  },
+  {
+    id: 6,
+    name: "エナジーバーナー",
+    src: "../public/monsters/energyburner.png",
+    feature:
+      "焼き肉のパワー全開！蓄電池体でエネルギー満タン、充電中のハイボルテージ・グリルビースト！",
+    status: {
+      hp: { jp: "体力", value: 63000 },
+      def: { jp: "防御力", value: 41000 },
+      att: { jp: "攻撃力", value: 58000 },
+      special: { jp: "資源回復力", value: 95000 },
+    },
+    mixup: {
+      parent: "ヤキニクカボモン",
+      word: "蓄電池",
     },
   },
 ];
