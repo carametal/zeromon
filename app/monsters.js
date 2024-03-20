53725a430a85ef92f2a3fe9aa5e922adcedd4314
mitsuki.fukuda <mitsuki.fukuda@g.smartvalue.ad.jp>
2024-03-20 11:56:06 +0900

    monster追加

---
+++ app/monsters.js
@@ -0,0 +1,37 @@
+export default Monsters = [
+  {
+    name: "タマネギカボモン",
+    src: "../public/monsters/tamanegi.png",
+    feature:
+      "このモンスターは日中、太陽光で充電し、夜になると光る玉ねぎの香りを放ちます。夢を育む不思議な生き物です。",
+    status: {
+      hp: { jp: "体力", value: 87700 },
+      def: { jp: "防御力", value: 22800 },
+      att: { jp: "攻撃力", value: 87700 },
+      special: { jp: "玉ねぎくささ", value: 93500 },
+    },
+  },
+  {
+    name: "ヤキニクカボモン",
+    src: "../public/monsters/yakiniku.png",
+    feature:
+      "このモンスターは焼き肉の夢を見るたびに香ばしい煙をふわりと出します。なんともいえない焼き肉の匂いが持ち味です。",
+    status: {
+      hp: { jp: "体力", value: 58800 },
+      def: { jp: "防御力", value: 71600 },
+      att: { jp: "攻撃力", value: 44600 },
+      special: { jp: "おいしさ", value: 34100 },
+    },
+  },
+  {
+    name: "カーリングカボモン",
+    src: "../public/monsters/carling.png",
+    feature: "",
+    status: {
+      hp: { jp: "体力", value: 45000 },
+      def: { jp: "防御力", value: 99999 },
+      att: { jp: "攻撃力", value: 21100 },
+      special: { jp: "つるつるさ", value: 68000 },
+    },
+  },
+];
